c237380f2b453e2173d4b4103a5280c3a241b726
Jpduenas17 <danpao45@gmail.com>
2022-08-18 22:20:55 -0500

    Implementar Logs

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
+import logging
 
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from prometheus_fastapi_instrumentator import Instrumentator 
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -27,3 +32,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+Instrumentator().instrument(app).expose(app)
